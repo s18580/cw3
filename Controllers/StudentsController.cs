280b2194855a071bd90ca94d444f174bf04a70bd
s18580 <s18580@pjwstk.edu.pl>
2020-03-19 15:39:25 +0100

    "Zadanie 4" - przekazywanie parametru jako URL segment

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -10,11 +10,24 @@ namespace cw3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
-       [HttpGet]
+        [HttpGet]
         public string GetStudents()
         {
             return "Kowalski, Malewski, Andrzejewski";
         }
 
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id==1)
+            {
+                return Ok("Kowalski");
+            }else if (id==2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
     }
 }
